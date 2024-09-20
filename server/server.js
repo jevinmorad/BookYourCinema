2267d41edb1f5e462ff4f3adc874000006000d0b
jevinmorad <23010101171@darshan.ac.in>
2024-09-20 15:41:58 +0530

    server

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ app.use(express.json())
 
 app.use('/movies', require('./routes/movie'));
 app.use('/theaters', require('./routes/theater'));
-app.use('/showtime',  require('./routes/showtime'))
+app.use('/showtime',  require('./routes/showtime'));
+app.use('/reservation',  require('./routes/reservation'));
 
 const port = process.env.PORT
 app.listen(port, ()=>{console.log(`Server running on port ${port}`);})
