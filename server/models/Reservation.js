2b2116626f64c75c3fc902b36ef12613a6e47c52
jevinmorad <23010101171@darshan.ac.in>
2024-09-23 15:52:28 +0530

    update

--- server/models/Reservation.js
+++ server/models/Reservation.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const BookingSchema = new Schema({
-    userId: {
+    users: {
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: true
+    },
+    movies: {
+        type: Schema.Types.ObjectId,
+        ref: 'Movie',
+        require: true
     },
     showtimeId: {
         type: Schema.Types.ObjectId,
