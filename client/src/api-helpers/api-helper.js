66bd2f9b2d0b32455b3ae09e09d6210b32d4f257
jevinmorad <23010101171@darshan.ac.in>
2024-10-10 21:09:52 +0530

    Add movie

--- client/src/api-helpers/api-helper.js
+++ client/src/api-helpers/api-helper.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const getAllMovies = async () => {
-    return await axios.get("/movies").catch((err) => console.log(err));
+    return await axios.get("/movies")
+        .catch((err) => console.error('Error during API request:', err.response?.data || err.message));
 };
 
 export const userAuthRequest = async (data, isSignIn) => {
@@ -21,14 +22,43 @@ export const userAuthRequest = async (data, isSignIn) => {
 
 export const adminAuthRequest = async (data, isSignIn) => {
     try {
-        const res = await axios.post(`/admin/${isSignIn ? "signin" : "signup"}` , {
+        const res = await axios.post(`/admin/${isSignIn ? "signin" : "signup"}`, {
             ...(isSignIn ? {} : { adminName: data.name, phone: data.mobile }),
             email: data.email,
             password: data.password,
         });
-        
+
         return res.data;
 
+    } catch (err) {
+        console.error('Error during API request:', err.response?.data || err.message);
+    }
+}
+
+export const addMovie = async (data) => {
+    console.log(data);
+
+    try {
+        const res = await axios.post(`/movies/`, {
+            title: data.title,
+            posterUrl: data.posterUrl,
+            trailerUrl: data.trailerUrl,
+            language: data.language,
+            cast: data.cast,
+            description: data.description,
+            genre: data.genre,
+            releaseDate: data.releaseDate,
+            duration: data.duration,
+            rating: data.rating,
+            admin: localStorage.getItem('adminId'),
+        },
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            });
+        console.log(res.data);
+        return res.data;
     } catch (err) {
         console.error('Error during API request:', err.response?.data || err.message);
     }
--- client/src/components/Movies/AddMovies.js
+++ client/src/components/Movies/AddMovies.js
@@ -1,103 +1,209 @@
+import { useState } from "react";
+import { addMovie } from "../../api-helpers/api-helper";
 
-export default function addMovie() {
+export default function AddMovie() {
     const [input, setInput] = useState({
-        name: "",
-        mobile: "",
-        email: "",
-        password: ""
-    });
+        title: "",
+        posterUrl: "",
+        trailerUrl: "",
+        language: [],
+        cast: [],
+        description: "",
+        genre: "",
+        releaseDate: "",
+        duration: "",
+        rating: "",
+    })
+
+    const [castArray, setCastArray] = new useState([]);
+    const [cast, setCast] = new useState("");
+    const [languageArray, setLanguageArray] = useState([]);
+    const [language, setLanguage] = new useState("");
+    const [genreArray, setGenreArray] = useState([]);
+    const [genre, setGenre] = new useState("");
 
     const handleChange = (e) => {
-        setInput((prevState) => ({
-            ...prevState,
+        setInput((prevInput) => ({
+            ...prevInput,
             [e.target.name]: e.target.value,
-        }));
+            language: languageArray,
+            cast: castArray,
+            genre: genreArray,
+        }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setInput((prevInput) => ({
+            ...prevInput,
+            language: languageArray,
+            cast: castArray,
+            genre: genreArray,
+        }))
+        console.log("input: ",input);
+        
+        addMovie({ ...input, languageArray, castArray, genreArray })
+            .then((res) => console.log(res))
+            .catch((err) => console.error(err.message))
     }
 
     return (
-        <div class="container">
-            <form>
-                <div class="mb-3">
-                    <label class="form-label">Title</label>
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label className="form-label">Title</label>
                     <input
                         type="text"
-                        class="form-control"
+                        className="form-control"
+                        name="title"
+                        value={input.title}
                         placeholder="Title of the movie"
-                        onClick={handleChange}
+                        onChange={handleChange}
                     />
                 </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Poster Url</label>
+                <div className="mb-3">
+                    <label className="form-label">Poster Url</label>
                     <input
                         type="text"
-                        class="form-control"
+                        className="form-control"
+                        name="posterUrl"
+                        value={input.posterUrl}
                         placeholder="Url of the poster"
-                        onClick={handleChange}
+                        onChange={handleChange}
                     />
                 </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Trailer Url</label>
+                <div className="mb-3">
+                    <label className="form-label">Trailer Url</label>
                     <input
                         type="text"
-                        class="form-control"
+                        className="form-control"
+                        name="trailerUrl"
+                        value={input.trailerUrl}
                         placeholder="Url of the trailer"
-                        onClick={handleChange}
+                        onChange={handleChange}
                     />
                 </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Language</label>
+                <div className="mb-3">
+                    <label className="form-label">Language</label>
                     <input
                         type="text"
-                        class="form-control"
+                        className="form-control"
+                        name="language"
+                        value={language}
                         placeholder="Language of movie"
-                        onClick={handleChange}
+                        onChange={(e) => setLanguage(e.target.value)}
                     />
+                    <button
+                        type="button"
+                        className="btn btn-primary mt-1"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setLanguageArray((prev) => [...prev, language]);
+                            setLanguage("")
+                        }}
+                    >
+                        Add
+                    </button>
                 </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Cast</label>
+                <div className="mb-3">
+                    <label className="form-label">Cast</label>
                     <input
                         type="text"
-                        class="form-control"
+                        className="form-control"
+                        name="cast"
+                        value={cast}
                         placeholder="Cast name"
-                        onClick={handleChange}
+                        onChange={(e) => setCast(e.target.value)}
                     />
+                    <button
+                        type="button"
+                        className="btn btn-primary mt-1"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setCastArray((prev) => [...prev, cast]);
+                            setCast("");
+                        }}
+                    >
+                        Add
+                    </button>
                 </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Description</label>
+                <div className="mb-3">
+                    <label className="form-label">Description</label>
                     <input
                         type="text"
-                        class="form-control"
+                        className="form-control"
+                        name="description"
+                        value={input.description}
                         placeholder="Description of the movie"
-                        onClick={handleChange}
+                        onChange={handleChange}
                     />
                 </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Release date</label>
+                <div className="mb-3">
+                    <label className="form-label">Genre</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="genre"
+                        value={genre}
+                        placeholder="Cast name"
+                        onChange={(e) => setGenre(e.target.value)}
+                    />
+                    <button
+                        type="button"
+                        className="btn btn-primary mt-1"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setGenreArray((prev) => [...prev, genre]);
+                            setGenre("");
+                        }}
+                    >
+                        Add
+                    </button>
+                </div>
+
+                <div className="mb-3">
+                    <label className="form-label">Release date</label>
                     <input
                         type="date"
-                        class="form-control"
+                        className="form-control"
+                        name="releaseDate"
+                        value={input.releaseDate}
                         placeholder="Release date"
-                        onClick={handleChange}
+                        onChange={handleChange}
                     />
                 </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Rating</label>
+                <div className="mb-3">
+                    <label className="form-label">Duration</label>
                     <input
                         type="text"
-                        class="form-control"
-                        placeholder="rating/10"
-                        onClick={handleChange}
+                        className="form-control"
+                        name="duration"
+                        value={input.duration}
+                        placeholder="duration minute"
+                        onChange={handleChange}
                     />
                 </div>
 
-                <button type="submit" class="btn btn-primary">
+                <div className="mb-3">
+                    <label className="form-label">Rating</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="rating"
+                        value={input.rating}
+                        placeholder="rating/10"
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
                     Submit
                 </button>
             </form>
